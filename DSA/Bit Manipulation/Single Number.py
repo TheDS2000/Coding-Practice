0f086d0b372c95f90a4d950990dcb8e9d7a3348f
Dhanashri_Shinde <50336672+TheDS2000@users.noreply.github.com>
2024-03-30 18:37:59 +0530

    Create Single Number.py

---
+++ DSA/Bit Manipulation/Single Number.py
@@ -0,0 +1,10 @@
+#TC : O(n), SC:O(1)
+#Using xor operation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+
+        return ans    
+        
