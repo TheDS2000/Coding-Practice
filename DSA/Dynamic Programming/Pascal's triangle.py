c3efc790eb0f95ea72ceac54f7278dd30a5cdf28
Dhanashri_Shinde <50336672+TheDS2000@users.noreply.github.com>
2024-04-09 21:09:01 +0530

    Update Pascal's triangle.py

--- DSA/Dynamic Programming/Pascal's triangle.py
+++ DSA/Dynamic Programming/Pascal's triangle.py
@@ -9,5 +9,23 @@ class Solution:
                 l[i][j]=l[i-1][j-1]+l[i-1][j]
         return l        
 
+Recursion:
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+        prevRows.append(newRow)
+        return prevRows
+
 
         
