ebe6b88c94a7704335b9ecfcd339cec1497f598d
Dhanashri_Shinde <50336672+TheDS2000@users.noreply.github.com>
2024-04-09 20:55:58 +0530

    Create Pascal's triangle.py

---
+++ DSA/Dynamic Programming/Pascal's triangle.py
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/pascals-triangle/description/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        l=[1]*numRows
+        for i in range(numRows):
+            l[i]=[1]*(i+1)
+            for j in range(1,i):
+                l[i][j]=l[i-1][j-1]+l[i-1][j]
+        return l        
+
+
+        
